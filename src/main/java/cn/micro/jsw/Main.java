3391e26327407d109365d0494c791992629fe50e
lry <595208882@qq.com>
2019-11-02 12:07:03 +0800

    init

---
+++ src/main/java/cn/micro/jsw/Main.java
@@ -0,0 +1,14 @@
+package cn.micro.jsw;
+
+/**
+ * The Start Main
+ *
+ * @author lry
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("The Start Main");
+    }
+
+}
